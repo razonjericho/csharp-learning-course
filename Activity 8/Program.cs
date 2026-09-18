f59fcffa41d8749452dfc3d691f34621597e36e2
Jericho Pete Razon <razonjerichopete@gmail.com>
2026-09-18 14:57:02 +0800

    Activity 8: Lists

---
+++ Activity 8/Program.cs
@@ -0,0 +1,93 @@
+﻿
+// List<string> favouriteLanguages = new List<string>
+// {
+//     "C#",
+//     "JavaScript",
+//     "Python"
+// };
+
+// favouriteLanguages.Add("Java");
+
+// for (int i = 0; i < favouriteLanguages.Count; i++) {
+//     Console.WriteLine(favouriteLanguages[i]);
+// }
+
+
+// List<string> favouriteLanguages = new List<string>
+// {
+//     "C#",
+//     "JavaScript",
+//     "Python"
+// };
+
+// favouriteLanguages.Remove("Python");
+
+// for (int i = 0; i < favouriteLanguages.Count; i++) {
+//     Console.WriteLine(favouriteLanguages[i]);
+// }
+
+// List<string> favouriteLanguages = new List<string>
+// {
+//     "C#",
+//     "JavaScript",
+//     "Python"
+// };
+
+// if (favouriteLanguages.Contains("C#")) {
+//     Console.WriteLine("C# is my favourite language.");
+// } else {
+//     Console.WriteLine("C# is not in my favourite languages.");
+// }
+
+// List<string> favouriteLanguages = new List<string>
+// {
+//     "C#",
+//     "JavaScript",
+//     "Python"
+// };
+
+// favouriteLanguages.RemoveAt(1);
+
+// for (int i = 0; i < favouriteLanguages.Count; i++) {
+//     Console.WriteLine(favouriteLanguages[i]);
+// }
+
+// List<string> favouriteLanguages = new List<string>
+// {
+//     "C#",
+//     "JavaScript",
+//     "Python"
+// };
+
+// favouriteLanguages.Insert(1, "Java");
+
+// for (int i = 0; i < favouriteLanguages.Count; i++) {
+//     Console.WriteLine(favouriteLanguages[i]);
+// }
+
+// List<string> favouriteLanguages = new List<string>
+// {
+//     "C#",
+//     "JavaScript",
+//     "Python"
+// };
+
+// Console.WriteLine($"I have {favouriteLanguages.Count} favourite languages.");
+
+List<string> favouriteLanguages = new List<string>
+{
+    "C#",
+    "JavaScript",
+    "Python"
+};
+
+Console.WriteLine($"Before clearing: {favouriteLanguages.Count}");
+
+favouriteLanguages.Clear();
+
+Console.WriteLine($"After clearing: {favouriteLanguages.Count}");
+
+
+
+
+
